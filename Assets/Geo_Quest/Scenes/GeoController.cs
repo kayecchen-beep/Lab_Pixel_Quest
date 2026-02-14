d6bf61fa050853dedc3b94b4e0972392ce81267e
kayecchen-beep <kaye.c.chen@gmail.com>
2026-02-14 16:29:14 -0500

    Biggerer changes
    code changes

---
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string valueOne = "Hello";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string valueOne = "World";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
