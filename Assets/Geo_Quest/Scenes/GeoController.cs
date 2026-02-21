aa2e931c58604ba1c254156c8b666635b6ab0146
kayecchen-beep <kaye.c.chen@gmail.com>
2026-02-21 16:24:29 -0500

    movement
    added movement :3

--- Assets/Geo_Quest/Scenes/GeoController.cs
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class GeoController : MonoBehaviour
 {
+    int var = 3;
     string valueOne = "Hello";
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World");
         string valueOne = "World";
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            transform.position += new Vector3(0, 1, 0);
+          
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) 
+        { 
+            transform.position += new Vector3(1, 0, 0); 
+        }
+            
+
     }
 }
